972ced41e4fb80a3dd96a602ac9fe5f3eeaa09d3
ardhiancalwa <ardhiancalwaofc@gmail.com>
2024-07-02 00:59:39 +0700

    hosting landingpage

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -66,7 +66,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "2d094dd43c5e5ddf5e73292e0c29b474
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "52480d33a4d927cf6587c47d071727c5",
+"flutter_bootstrap.js": "5f5be7cfc3c5ff9cf8c244bc442895d1",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
